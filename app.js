8a5f4c596f90173380f0e6379fa02eb8a5e47b3b
Robyn Navarro <rnavarro182@gmail.com>
2018-05-07 16:17:48 -0700

    added css file and finished app.js

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ function showSum() {
     document.getElementById('sum-total').innerHTML = result.value;
     document.getElementById('sum-description').innerHTML = result.description;
 }
+
+function showDifference() {
+    var x = parseInt(document.getElementById('number-to-sub-1').value);
+    var y = parseInt(document.getElementById('number-to-sub-2').value);
+    var result = subtract(x, y);
+    document.getElementById('difference').innerHTML = result.value;
+    document.getElementById('subtract-description').innerHTML = result.description;
+}
+
+function showProduct() {
+    var x = parseInt(document.getElementById('factor-1').value);
+    var y = parseInt(document.getElementById('factor-2').value);
+    var result = multiply(x, y);
+    document.getElementById('product').innerHTML = result.value;
+    document.getElementById('multiply-description').innerHTML = result.description;
+}
+
+function showQuotient() {
+    var x = parseInt(document.getElementById('dividend').value);
+    var y = parseInt(document.getElementById('divisor').value);
+    var result = divide(x, y);
+    document.getElementById('quotient').innerHTML = result.value;
+    document.getElementById('divide-description').innerHTML = result.description;
+
+}
