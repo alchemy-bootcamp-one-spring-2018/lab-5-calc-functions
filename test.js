663a8519b1833a83004813826e4f3f7462630c4d
Mark Myers <markalope@gmail.com>
2018-05-07 14:37:07 -0700

    testing test.html

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-
+//values for test functions will be 21 and 5
 
 //test for addition function
-function testdisplayRemainder() {
-    const result = displayRemainder(29, 6);
-    if(result.value !== 5) {
-        console.error('testRemainder - value - FAIL', result.value);
+function testdisplayAddSum() {
+    const result = displayAddSum(21, 5);
+    if(result.value !== 26) {
+        console.error('testdisplayAddSum - value - FAIL', result.value);
     } else {
-        console.log('testRemainder - value - SUCCESS');
+        console.log('testdisplayAddSum - value - SUCCESS');
     }
 }
 
