7c2850b8846b6a78ea1829a8d9964dd43f6c81d8
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-05-07 15:26:11 -0700

    Add more calc functions and stretch goals

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-/* exported add, subtract, multiply, divide */
+/* exported add, subtract, multiply, divide, divideWithRemainder, hypotenuse, isEven, isOdd */
 
 function add(x, y) {
     const sum = x + y;
@@ -30,8 +30,78 @@ function multiply(x, y) {
 function divide(x, y) {
     const quotient = x / y;
 
+    if(y === 0) {
+        return {
+            value: 'To infinity and beyond!',
+            description: 'Error! Can\'t divide by 0!',
+        };
+    } else {
+        return {
+            value: quotient,
+            description: 'The quotient of ' + x + ' and ' + y + ' is: ' + quotient
+        };
+    }
+}
+
+function divideWithRemainder(x, y) {
+    const quotient = Math.trunc(x / y);
+    const remainder = x % y;
+
+    if(y === 0) {
+        return {
+            value: 'To infinity and beyond!',
+            description: 'Error! Can\'t divide by 0!',
+        };
+    } else {
+        return {
+            firstValue: quotient,
+            secondValue: remainder,
+            description: 'The quotient of ' + x + ' and ' + y + ' is ' + quotient + ', with a remainder of ' + remainder
+        };
+    }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
+function hypotenuse(x, y) {
+    const hypo = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
     return {
-        value: quotient,
-        description: 'The quotient of ' + x + ' and ' + y + ' is: ' + quotient
+        value: hypo,
+        description: 'The hypotenuse of a right triangle with side lengths ' + x + ' and ' + y + ' is: ' + hypo
     };
+}
+
+function isEven(x) {
+    const even = x % 2;
+
+    if(even === 0) {
+        return {
+            value: 'Yep!',
+            description: x + ' is even!'
+        };
+    }
+    else {
+        return {
+            value: 'No!',
+            description: x + ' is NOT even!'
+        };
+    }
+}
+
+function isOdd(x) {
+    const odd = x % 2;
+
+    if(odd === 1) {
+        return {
+            value: 'Yep!',
+            description: x + ' is odd!'
+        };
+    }
+    else {
+        return {
+            value: 'No!',
+            description: x + ' is NOT odd!'
+        };
+    }
 }
--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-/* exported testAdd, testSubtract, testMultiply, testDivide */
-/* globals add, subtract, multiply, divide */
+/* globals add, subtract, multiply, divide, divideWithRemainder, hypotenuse, isEven, isOdd */
 
 function testAdd() {
     const result = add(15, 21);
@@ -41,7 +40,51 @@ function testDivide() {
     }
 }
 
+function testDivideWithRemainder() {
+    const result = divideWithRemainder(42, 5);
+    if(result.firstValue !== 8 && result.secondValue !== 2) {
+        console.error('testDivideWithRemainder - value - FAIL', result.value);
+    }
+    else {
+        console.log('testDivideWithRemainder - value - SUCCESS');
+    }
+}
+
+function testHypotenuse() {
+    const result = hypotenuse(3, 4);
+    if(result.value !== 5) {
+        console.error('testHypotenuse - value - FAIL', result.value);
+    }
+    else {
+        console.log('testHypotenuse - value - SUCCESS');
+    }
+}
+
+function testIsEven() {
+    const result = isEven(8);
+    if(result.value === 1) {
+        console.error('testIsEven - value - FAIL', result.value);
+    }
+    else {
+        console.log('testIsEven - value - SUCCESS');
+    }
+}
+
+function testIsOdd() {
+    const result = isOdd(9);
+    if(result.value === 0) {
+        console.error('testIsOdd - value - FAIL', result.value);
+    }
+    else {
+        console.log('testIsOdd - value - SUCCESS');
+    }
+}
+
 testAdd();
 testSubtract();
 testMultiply();
 testDivide();
+testDivideWithRemainder();
+testHypotenuse();
+testIsEven();
+testIsOdd();
