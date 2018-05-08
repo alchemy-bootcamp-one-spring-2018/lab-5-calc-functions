29075aa59347d88df44be20999e23456156e7d84
Erik Withak <ErikAanestad@gmail.com>
2018-05-08 09:17:05 -0700

    removed "value" from fail/success messages

--- test.js
+++ test.js
@@ -1,41 +1,40 @@
 // Test some shizz
 function testSum() {
-    const result = sum(29, 6);
+    const result = sum(29, 1);
     if(result.value !== 35) {
-        console.error('testSum - value - FAIL', result.value);
     }
     else {
-        console.log('testSum - value - SUCCESS');
+        console.log('testSum - SUCCESS');
     }
 }
 
 function testMinus() {
     const result = minus(29, 6);
     if(result.value !== 23) {
-        console.error('testMinus - value - FAIL', result.value);
+        console.error('testMinus - FAIL', result.value);
     }
     else {
-        console.log('testMinus - value - SUCCESS');
+        console.log('testMinus - SUCCESS');
     }
 }
 
 function testProduct() {
     const result = product(25, 4);
     if(result.value !== 100) {
-        console.error('testProduct - value - FAIL', result.value);
+        console.error('testProduct - FAIL', result.value);
     }
     else {
-        console.log('testProduct - value - SUCCESS');
+        console.log('testProduct - SUCCESS');
     }
 }
 
 function testDivide() {
     const result = divide(25, 5);
     if(result.value !== 5) {
-        console.error('testDivide - value - FAIL', result.value);
+        console.error('testDivide - FAIL', result.value);
     }
     else {
-        console.log('testDivide - value - SUCCESS');
+        console.log('testDivide - SUCCESS');
     }
 }
 
@@ -43,10 +42,10 @@ function testDivide() {
 function testRemainder() {
     const result = remainder(27, 9);
     if(result.value !== 0) {
-        console.error('testRemainder - value - FAIL', result.value);
+        console.error('testRemainder - FAIL', result.value);
     }
     else {
-        console.log('testRemainder - value - SUCCESS');
+        console.log('testRemainder - SUCCESS');
     }
 }
 
