fe9700b9f154d0f07b5e3f86829c816a73f2cfef
Rehmsy <sarahrehmer@gmail.com>
2018-05-07 17:46:57 -0700

    add, subtract, mult, divide, working. need to add descriptive text and move some items from test to production files

--- test.js
+++ test.js
@@ -1,18 +1,5 @@
 'use strict';
 
-function showMultiply() {
-    var result;
-    var x = parseInt(document.getElementById('multiply').value);
-    var y = parseInt(document.getElementById('multiplyBy').value);
-    result = x * y;
-    document.getElementById('multiplyEquals').textContent = result.value;
-    document.getElementById('multiply-description').textContent = result;
-    console.log('hi');
-    console.log(x);
-    console.log(y);
-    console.log(x * y);
-}
-
 function showAddition() {
     var result;
     var x = parseInt(document.getElementById('add1').value);
@@ -20,7 +7,45 @@ function showAddition() {
     result = x + y;
     document.getElementById('addEquals').textContent = result.value;
     document.getElementById('add-description').textContent = result;
+    console.log(x);
+    console.log(y);
+    console.log(x + y);
+}
 
+function showSubtraction() {
+    var result;
+    var x = parseInt(document.getElementById('subtract1').value);
+    var y = parseInt(document.getElementById('subtract2').value);
+    result = x - y;
+    document.getElementById('subtractEquals').tectContent = result.value;
+    document.getElementById('subtract-description').textContent = result;
+    console.log(x);
+    console.log(y);
+    console.log(x - y);
+}
+
+function showMultiply() {
+    var result;
+    var x = parseInt(document.getElementById('multiply1').value);
+    var y = parseInt(document.getElementById('multiply2').value);
+    result = x * y;
+    document.getElementById('multiplyEquals').textContent = result.value;
+    document.getElementById('multiply-description').textContent = result;
+    console.log(x);
+    console.log(y);
+    console.log(x * y);
+}
+
+function showDivision() {
+    var result;
+    var x = parseInt(document.getElementById('divide1').value);
+    var y = parseInt(document.getElementById('divide2').value);
+    result = x / y;
+    document.getElementById('divideEquals').textContent = result.value;
+    document.getElementById('divide-description').textContent = result;
+    console.log(x);
+    console.log(y);
+    console.log(x / y);
 }
 
 
